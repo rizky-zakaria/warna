540e18fea93aafaa2e2ef3ddb93c99253e818462
Rizky-zakaria <rizky.zakaria78@gmail.com>
2020-02-29 02:19:44 +0800

    berhasil di tambahkan

---
+++ application/controllers/User.php
@@ -0,0 +1,14 @@
+<?php
+
+use Restserver\Libraries\REST_Controller;
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+class User extends REST_Controllerller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
