c4b78d7ffd92865e295705154ad8bc7e2e6d191b
Rizky-zakaria <rizky.zakaria78@gmail.com>
2020-02-29 09:42:47 +0800

    ubah

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -20,12 +20,12 @@ class Auth extends REST_Controller
 
         // Configure limits on our controller methods
         // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
-        $this->methods['users']['limit'] = 500; // 500 requests per hour per user/key
+        $this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
         $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
         $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
     }
 
-    public function users()
+    public function users_get()
     {
         // Users from a data store e.g. database
         $users = [
