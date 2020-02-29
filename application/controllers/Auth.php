f7775a485407e9acc99d2ddeceac6821b6dd33c1
Rizky-zakaria <rizky.zakaria78@gmail.com>
2020-02-29 09:34:14 +0800

    ubah

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -28,7 +28,7 @@ class Auth extends REST_Controller
     public function users_get()
     {
         // Users from a data store e.g. database
-        $users = [
+        $user = [
             ['id' => 1, 'name' => 'John', 'email' => 'john@example.com', 'fact' => 'Loves coding'],
             ['id' => 2, 'name' => 'Jim', 'email' => 'jim@example.com', 'fact' => 'Developed on CodeIgniter'],
             ['id' => 3, 'name' => 'Jane', 'email' => 'jane@example.com', 'fact' => 'Lives in the USA', ['hobbies' => ['guitar', 'cycling']]],
